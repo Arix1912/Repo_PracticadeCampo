a89b1695085fcb7a46c656a9c1cf60c72860c8dd
Wilmer Abanto <=>
2025-05-03 17:06:13 -0500

    actualización claculadora

--- proyecto.py
+++ proyecto.py
@@ -2,5 +2,6 @@ print("Bienvenidos a la calculadora suma")
 x = input("ingrese primer numero")
 y = input("ingrese segundo numero")
 z = input("ingrese tercer numero")
-suma = x+y+z
+def suma (x,y,z):
+    return x+y+z
 print("Resultado: ",suma)
